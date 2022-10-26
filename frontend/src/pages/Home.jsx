304d0cda14e50e298d30b1dedede88ceb8db27a6
mlgs <mathieu.langlois@voxm.live>
2022-10-26 12:45:44 +0200

    Create first part of homepage 'header-quizz-block'

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,36 +1,21 @@
-import Counter from "@components/Counter";
-import logo from "@assets/logo.svg";
+import LevelButton from "@components/LevelButton";
+// eslint-disable-next-line no-unused-vars
+import header from "@assets/header-quizz.png";
+import "./Home.css";
 
 export default function Home() {
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Hello Vite + React !</p>
-
-      <Counter />
-
-      <p>
-        Edit <code>App.jsx</code> and save to test HMR updates.
-      </p>
-      <p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {" | "}
-        <a
-          className="App-link"
-          href="https://vitejs.dev/guide/features.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vite Docs
-        </a>
-      </p>
-    </header>
+    <>
+      <header className="Quizz-header">
+        <h1>
+          Testez vos connaissances du système solaire ! Jouez maintenant :
+        </h1>
+        <LevelButton />
+      </header>
+      <div className="scores-table">
+        <h2>MEILLEURS SCORES</h2>
+        *Choir niveau* *Tableau des scores*
+      </div>
+    </>
   );
 }
