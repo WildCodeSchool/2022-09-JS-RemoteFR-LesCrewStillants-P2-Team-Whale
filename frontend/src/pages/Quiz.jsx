74b53f18c08ecde0e68ae88e615064ce54c6e35a
saigyou_ayakashi <minuit1993@live.fr>
2022-10-27 17:28:11 +0200

    Create home quiz page (mobile and desktop) style

--- frontend/src/pages/Quiz.jsx
+++ frontend/src/pages/Quiz.jsx
@@ -1,19 +1,22 @@
-import './../assets/css/Quiz.css';
+import "@assets/css/Quiz.css";
+import logo from "@assets/logo.png";
 
 export default function Quiz() {
   return (
-    <>
-      <section className="quiz"> 
-        <img src="./img/logo.png" alt="Quiz" className="logo" />
-        <div className="quizContainer">
-          <p>Hello aventurier de l'espace</p>
-          <p>Choisissez votre difficultée</p>
-          <div className="buttonContainer">
-            <button className="buttonDifficulty">Facile</button>
-            <button className="buttonDifficulty">Difficile</button>
-          </div>
+    <section className="quiz">
+      <img src={logo} alt="Quiz" className="logo" />
+      <div className="quizContainer">
+        <p>Hello aventurier de l'espace</p>
+        <p>Choisissez votre difficultée</p>
+        <div className="buttonContainer">
+          <button type="submit" className="buttonDifficulty">
+            Facile
+          </button>
+          <button type="submit" className="buttonDifficulty">
+            Difficile
+          </button>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }
