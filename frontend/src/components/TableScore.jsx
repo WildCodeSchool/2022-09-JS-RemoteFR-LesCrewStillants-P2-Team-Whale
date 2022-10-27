f97e341dc7c75dc75e9d0f02252d51d08f93c259
mlgs <mathieu.langlois@voxm.live>
2022-10-27 16:43:15 +0200

    Create Tables of score for homepage + Add mui dep

--- frontend/src/components/TableScore.jsx
+++ frontend/src/components/TableScore.jsx
@@ -1,19 +1,22 @@
 import { Tab } from "@headlessui/react";
 import "./TableScore.css";
-import EasyScores from "@components/Scores";
+import TableDifficult from "@components/TableDifficult";
+import EasyTable from "@components/TableEasy";
 
 export default function MyTabs() {
   return (
     <Tab.Group>
-      <Tab.List class="selectlevel">
-        <Tab class="level">Facile</Tab>
-        <Tab class="level">Difficile</Tab>
+      <Tab.List className="selectlevel">
+        <Tab className="level">Facile</Tab>
+        <Tab className="level">Difficile</Tab>
       </Tab.List>
-      <Tab.Panels class="tablescores">
+      <Tab.Panels className="tablescores">
         <Tab.Panel>
-          <EasyScores />
+          <EasyTable />
         </Tab.Panel>
-        <Tab.Panel>Content 2</Tab.Panel>
+        <Tab.Panel>
+          <TableDifficult />
+        </Tab.Panel>
       </Tab.Panels>
     </Tab.Group>
   );
