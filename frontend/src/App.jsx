528be5f7091e5a8838f44f92414f1a6558473fa7
Yohan0412 <Yohan0412>
2022-10-28 09:53:36 +0200

    premiercommit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import Home from "@pages/Home";
-
+import Footer from "@pages/footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <p>coucou</p>
+      <Footer />
     </div>
   );
 }
