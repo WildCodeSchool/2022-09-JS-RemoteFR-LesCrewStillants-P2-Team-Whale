71830f855083e4e5e426992e7464a33c65328429
saigyou_ayakashi <minuit1993@live.fr>
2022-10-28 10:01:44 +0200

    fix app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import Quiz from "@pages/Quiz";
+import Home from "@pages/Home";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Quiz />
+      <Home />
     </div>
   );
 }
