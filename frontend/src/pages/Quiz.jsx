7fe9c20735bef7cdf7c29836c57dd8a2368ef6c3
saigyou_ayakashi <minuit1993@live.fr>
2022-10-28 09:44:17 +0200

    Add logo (lottifile) for home quiz

--- frontend/src/pages/Quiz.jsx
+++ frontend/src/pages/Quiz.jsx
@@ -1,11 +1,22 @@
-import "@assets/css/Quiz.css";
+import { Player } from '@lottiefiles/react-lottie-player';
+
 import logo from "@assets/logo.png";
+import lotti from "@assets/lotti.json";
+
+import "@assets/css/Quiz.css";
 
 export default function Quiz() {
   return (
     <section className="quiz">
       <img src={logo} alt="Quiz" className="logo" />
       <div className="quizContainer">
+      <Player
+        autoplay
+        loop
+        src={lotti}
+        style={{ height: '300px', width: '300px' }}
+      >
+      </Player>
         <p>Hello aventurier de l'espace</p>
         <p>Choisissez votre difficultée</p>
         <div className="buttonContainer">
