0f666707591c579695fc61bec4497ac3c73c5324
saigyou_ayakashi <minuit1993@live.fr>
2022-10-29 13:41:08 +0200

    Fixe css, using lazyLoading for react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,36 @@
+import React from "react";
+
 import { Routes, Route } from "react-router-dom";
 
-import Home from "@pages/Home";
-import Quiz from "@pages/Quiz";
-
 import "./App.css";
+
+const Home = React.lazy(() => import("@pages/Home"));
+const Quiz = React.lazy(() => import("@pages/Quiz"));
 
 function App() {
   return (
     <div className="App">
       {/* <Routes> is a React Router component that allows you to define multiple routes. */}
       <Routes>
-        {/* <Route> is a React Router component that allows you to define a route. */}
-        <Route path="/" element={<Home />} />
-        <Route path="quiz" element={<Quiz />} />
+        {/* <Route> is a React Router component that allows you to define a single route. */}
+        {/* <React.Suspense> is a React component
+        that allows you to define a fallback component while the component is loading. */}
+        <Route
+          index
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <Home />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="quiz"
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <Quiz />
+            </React.Suspense>
+          }
+        />
       </Routes>
     </div>
   );
