0b31fb075e7d6df5fc4f6755156d4d122217f153
saigyou_ayakashi <minuit1993@live.fr>
2022-10-29 12:10:58 +0200

    Create route with 'React.router'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
 import Home from "@pages/Home";
+import Quiz from "@pages/Quiz";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Routes> is a React Router component that allows you to define multiple routes. */}
+      <Routes>
+        {/* <Route> is a React Router component that allows you to define a route. */}
+        <Route path="/" element={<Home />} />
+        <Route path="quiz" element={<Quiz />} />
+      </Routes>
     </div>
   );
 }
