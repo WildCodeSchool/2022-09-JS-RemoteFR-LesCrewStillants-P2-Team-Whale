356a92870d90fb905a9187fbe4f3607f5457f928
VICTOIREMICHELDLM <victoiremicheldlm@gmail.com>
2022-11-04 12:16:22 +0100

    responsive et orthographe

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -1,45 +1,40 @@
 import planet from "@assets/planet.png";
 
-import "./contactForm.css";
+import "./ContactForm.css";
 
 export default function ContactForm() {
   return (
-    <div className="contact">
-      <div className="container">
-        <div className="form-container">
-          <form className="form-box">
-            <h1>
-              <span>
-                Contacter Nous
-                <img className="img-alien" src={planet} alt="Visage_Alien" />
-              </span>
-            </h1>
-            <div>
-              <span>Nom *</span>
-              <input type="text" placeholder="Enter your name" />
-            </div>
-            <div>
-              <span>Email *</span>
-              <input type="text" placeholder="example@email.com" />
-            </div>
-            <div>
-              <span>Numéro</span>
-              <input type="text" placeholder="06060606" />
-            </div>
-            <div>
-              <span>Sujet*</span>
-              <input type="text" placeholder="Meilleur Quizz" />
-            </div>
-            <div>
-              <span>Message*</span>
-              <input type="text" placeholder="Votre message ici" />
-            </div>
-            <button type="submit" className="buttonSubmit">
-              Submit
-            </button>
-          </form>
+    <div className="container">
+      <form className="form-box">
+        <span>
+          <h1>Contactez Nous</h1>
+          <img className="img-alien" src={planet} alt="Visage_Alien" />
+        </span>
+
+        <div>
+          <span>Nom*</span>
+          <input type="text" placeholder="Enter your name" />
         </div>
-      </div>
+        <div>
+          <span>Email*</span>
+          <input type="text" placeholder="example@email.com" />
+        </div>
+        <div>
+          <span>Numéro*</span>
+          <input type="text" placeholder="06060606" />
+        </div>
+        <div>
+          <span>Sujet*</span>
+          <input type="text" placeholder="Best Quiz thank you" />
+        </div>
+        <div>
+          <span>Message*</span>
+          <input type="text" placeholder="Votre message ici" />
+        </div>
+        <button type="submit" className="buttonSubmit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
