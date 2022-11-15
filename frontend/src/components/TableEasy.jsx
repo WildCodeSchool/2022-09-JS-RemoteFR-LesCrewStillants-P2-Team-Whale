2cd7b3b2c58309a3f0beff4004f72087906f6a9a
mlgs <mathieu.langlois@voxm.live>
2022-11-15 09:40:05 +0100

    fix size of table

--- frontend/src/components/TableEasy.jsx
+++ frontend/src/components/TableEasy.jsx
@@ -18,7 +18,7 @@ export default function TableEasy() {
   }, []);
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table sx={{ minWidth: 460 }} aria-label="simple table">
         <TableBody>
           {!score && <div>Chargement</div>}
           {score &&
