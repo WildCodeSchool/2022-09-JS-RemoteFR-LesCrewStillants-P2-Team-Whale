21bb60bec146f86e5d913d9c0ba7f35aac97272e
mlgs <mathieu.langlois@voxm.live>
2022-11-16 11:20:29 +0100

    Modify logic quiz and css

--- frontend/src/components/Quiz/QuizQuestion.jsx
+++ frontend/src/components/Quiz/QuizQuestion.jsx
@@ -1,14 +1,16 @@
 import { Player } from "@lottiefiles/react-lottie-player";
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import questionService from "@services/QuestionService";
+import book from "@assets/lottie-file/book.json";
 import { API_BASE_URL } from "../../axios/AppConfig";
 import "@assets/css/QuizQuestion.css";
 
 export default function QuizQuestion({ difficulty }) {
   const [question, setQuestion] = useState(undefined);
   const [questionIndex, setQuestionIndex] = useState(0);
-  const navigate = useNavigate();
+  const [ShowResult, setShowResult] = useState(false);
+  const [score, setScore] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState(false);
 
   useEffect(() => {
     if (difficulty === "easy") {
@@ -22,46 +24,84 @@ export default function QuizQuestion({ difficulty }) {
     }
   }, [difficulty]);
 
-  const handleClick = () => {
+  const handleAnswer = () => {
+    setSelectedAnswer(true);
+  };
+
+  const handleClick = (correctAnswer) => {
+    if (correctAnswer) {
+      setScore(score + 1);
+    }
     if (questionIndex + 1 < 10) {
       setQuestionIndex(questionIndex + 1);
     } else {
-      navigate("/quiz/score");
+      setShowResult(true);
     }
   };
 
   return (
     <>
-      {!question && <div>loading...</div>}
-      {question && (
+      {ShowResult ? (
         <>
+          {/* <Player> is a component from the lottiefiles library
+      i using this 'player' for the picture animation */}
           <Player
             autoplay
             loop
-            src={`${API_BASE_URL}/lottie/${question[questionIndex].lottie}`}
+            src={book} // <- or not book
             style={{ height: "150px", width: "150px" }}
           />
-          <p>{question[questionIndex].quest}</p>
-          <div className="buttonAnswerContainer">
-            {question[questionIndex].answers.map((answer) => (
-              <button
-                key={answer}
-                type="submit"
-                onClick={handleClick}
-                className="answerButton"
-              >
-                {answer}
-              </button>
-            ))}
-          </div>
-          <div className="buttonResetContainer">
-            <button type="button" className="resetButton">
-              Reset
+          <p className="score">Your score</p>
+          <p className="score">{score} / 10</p>
+          <p>Almost there !</p>
+          <input className="inputName" type="text" placeholder="Your name" />
+          <div className="buttonScoreContainer">
+            <button type="submit" className="buttonSubmit">
+              Submit
             </button>
-            <p>{questionIndex + 1}/10</p>
+            <button type="submit" className="buttonScore" onClick={handleClick}>
+              Accueil
+            </button>
           </div>
         </>
+      ) : (
+        <>
+          {!question && <div>loading...</div>}
+          {question && (
+            <>
+              <Player
+                autoplay
+                loop
+                src={`${API_BASE_URL}/lottie/${question[questionIndex].lottie}`}
+                style={{ height: "150px", width: "150px" }}
+              />
+              <p>{question[questionIndex].quest}</p>
+              <div className="buttonAnswerContainer">
+                {question[questionIndex].answers.map((answer) => (
+                  <button
+                    key={answer}
+                    type="submit"
+                    onClick={handleAnswer}
+                    className={
+                      selectedAnswer ? "answerButtonSelected" : "answerButton"
+                    }
+                  >
+                    {answer}
+                  </button>
+                ))}
+              </div>
+              <div className="buttonResetContainer">
+                <button type="button" className="resetButton">
+                  Valider
+                </button>
+                <p>{questionIndex + 1}/10</p>
+                <p>Mon score : {score}</p>
+              </div>
+            </>
+          )}
+        </>
       )}
+      )
     </>
   );
 }
